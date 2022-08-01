f05fafdcfb8c9c96b057431375f9e0426a05e136
Kang Juhyeong <kakao_panda@naver.com>
2022-08-02 08:47:14 +0900

    20220802

---
+++ D1/2047. 신문 헤드라인.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner s = new Scanner(System.in);
+		String input = s.next();
+		String transform = input.toUpperCase();
+		System.out.println(transform);
+	}
+}
