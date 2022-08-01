899f9ebb9b3ce7c9542b90d35705203d6469c879
Kang Juhyeong <kakao_panda@naver.com>
2022-08-02 08:48:41 +0900

    20220802

---
+++ D1/2046. 스탬프 찍기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner s = new Scanner(System.in);
+		int input = s.nextInt();
+		for(int i=0; i<input; i++) {
+			System.out.print('#');
+		}
+	}
+}
