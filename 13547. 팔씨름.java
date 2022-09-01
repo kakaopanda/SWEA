d80a4eff2fce94598c244d809d3936c68a5acf99
Kang Juhyeong <kakao_panda@naver.com>
2022-09-02 08:38:34 +0900

    20220902

---
+++ 13547. 팔씨름.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner s = new Scanner(System.in);
+		int testcase = s.nextInt();
+		String result[] = new String[testcase];
+		for(int i=0; i<testcase; i++) {
+			String str = s.next();
+			char array[] = str.toCharArray();
+			int count_o = 0;
+			int count_x = 0;
+			for(int j=0; j<array.length; j++) {
+				if(array[j]=='o')
+					count_o++;
+				if(array[j]=='x')
+					count_x++;
+			}
+			String answer = "";
+			if(count_x <8)
+				answer = "YES";
+			else
+				answer = "NO";
+			result[i] = answer;
+			}
+		for(int i=0; i<testcase; i++) {
+			System.out.println("#"+(i+1)+" "+result[i]);
+		}
+	}
+}
