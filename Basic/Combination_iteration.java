af2452a677ece483999dbf0fd3e5b5fd5ab06ad9
Kang Juhyeong <kakao_panda@naver.com>
2022-10-05 10:14:51 +0900

    20221005

---
+++ Basic/Combination_iteration.java
@@ -0,0 +1,14 @@
+// 반복문을 이용하여 조합(Combination)을 구성하는 방법
+// N개의 숫자를 대상으로 조합을 구성할 경우, N번 중첩된 for문의 형태를 갖게 된다.
+// 이때, 다음번 인덱스는 이전 인덱스의 영향을 받게 된다.
+public class Combination_iteration {
+	public static void main(String[] args) {
+		for(int i=1; i<=4; i++) {
+			for(int j=i+1; j<=4; j++) {
+				for(int k=j+1; k<=4; k++) {
+					System.out.println(i+","+j+","+k);
+				}
+			}
+		}
+	}
+}
